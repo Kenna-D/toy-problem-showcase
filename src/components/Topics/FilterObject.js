87133359a516edd2233db57e62dc895614229e5e
Makenna Garrett <kenna.dunford@gmail.com>
2021-03-31 13:25:50 -0600

    completed all toys

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
 
 export default class FilterObject extends Component {
-  render(){
-    return (
-      <div className='puzzleBox filterObjectPB'>
-        <h4> Filter Object </h4>
-        <span className='puzzleText'>Original: { JSON.stringify(this.state.unFilteredArray, null, 10) }</span>
-        <input className='inputLine' onChange={(e) => this.handleChange(e.target.value) }/>
-        <button className='confirmationButton' onClick={ () => this.filterEmployees(this.state.userInput) } >Filter</button>
-        <span className='resultsBox filterObjectRB'> Filtered: { JSON.stringify(this.state.filteredEmployees, null, 10) }</span>
-      </div>
-    )
-  }
-
+  
   constructor(){
     super();
     this.state = {
@@ -36,20 +25,32 @@ export default class FilterObject extends Component {
       filteredArray: []
     }
   };
-
+  
   handleChange(val){
     this.setState({ userInput: val });
   }
-
+  
   filterEmployees(prop){
-    let employees = this.state.employees;
+    let employees = this.state.unFilteredArray;
     let filteredEmployees = [];
-
+    
     for(let i = 0; i < employees.length; i++){
-      if(employees[i].hasOwnProperty(prop))
-      filteredEmployees.push(employees[i])
+      if(employees[i].hasOwnProperty(prop)) {
+        filteredEmployees.push(employees[i])
+      }
     }
     this.setState({ filteredEmployees: filteredEmployees })
+  }
 
+  render(){
+    return (
+      <div className='puzzleBox filterObjectPB'>
+        <h4> Filter Object </h4>
+        <span className='puzzleText'>Original: { JSON.stringify(this.state.unFilteredArray, null, 10) }</span>
+        <input className='inputLine' onChange={(e) => this.handleChange(e.target.value) }/>
+        <button className='confirmationButton' onClick={ () => this.filterEmployees(this.state.userInput) } >Filter</button>
+        <span className='resultsBox filterObjectRB'> Filtered: { JSON.stringify(this.state.filteredEmployees, null, 10) }</span>
+      </div>
+    )
   }
 }
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Sum extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      num1: 0,
+      num2: 0,
+      sum: null
+    };
+  };
+
+  handleChangeOne(value){
+    this.setState({num1: parseInt(value, 10)});
+  };
+  handleChangeTwo(value){
+    this.setState({num1: parseInt(value, 10)});
+  };
+
+  add(num1, num2){
+    this.setState({sum: num1 + num2});
+  };
+
   render(){
     return (
-      <p> Sum Component</p>
+      <div className="puzzleBox sumPB">
+        <h4>Sum</h4>
+        <input className="inputLine" onChange={(e) => this.handleChangeOne(e.target.value)}/>
+        <input className="inputLine" onChange={(e) => this.handleChangeTwo(e.target.value)}/>
+        <button className="confirmationButton" onClick={() => this.add(this.state.num1, this.state.num2)}>Add</button>
+        <span className="resultsBox">Sum: {this.state.sum}</span>
+      </div>
+      
     )
   }
 }
